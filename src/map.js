162d0366e1c9c65dd3d72e4d088563a604d606ea
Mauro <maurocabreramolinaa29@gmail.com>
2026-07-19 12:02:20 -0300

    creación botón de descarga

--- src/map.js
+++ src/map.js
@@ -14,7 +14,7 @@ let baseLayers = {
 
 let puntos_sci = L.featureGroup()
 
-function asignarBoton(id_html) {
+function asignarBotonSimbolo(id_html) {
     document.getElementById(id_html).addEventListener("click", ev => {
         let iconSize = [30, 30];
         if (id_html == "sci_pc") {   // sci_pc.png tiene una forma particular.
@@ -24,19 +24,38 @@ function asignarBoton(id_html) {
             iconUrl: `icons/${id_html}.png`,
             iconSize: iconSize
         });
-        L.marker([-39, -64], {
+        let punto = L.marker([-39, -64], {
             icon: icon
         }).bindPopup("Puesto de comando").addTo(puntos_sci).addTo(map);
+        // verCapas(puntos_sci)
+        descargarPunto(punto)
     })
 }
 
+let btn_descargar = document.getElementById("btn_descargar").addEventListener("click",function(ev){
+    descargarGroupLayer(puntos_sci);
+})
+
 function verCapas(mapa) {
     mapa.eachLayer(function (layer) {
-        console.log(layer)
+        let geojson = layer.toGeoJSON();
+        console.log("Geojson",geojson)
     })
 }
-asignarBoton("sci_pc");
-asignarBoton("sci_e");
+
+function descargarGroupLayer(groupLayer){
+    let geojson = groupLayer.toGeoJSON();
+    let blob = new Blob([JSON.stringify(geojson)],{type:"application/json"})
+    let url = URL.createObjectURL(blob);
+    let a = document.createElement("a");
+    a.href=url
+    a.download = "DESCARGAR";
+    a.click()
+    console.log("Descargando...")
+}
+
+asignarBotonSimbolo("sci_pc");
+asignarBotonSimbolo("sci_e");
 
 L.control.scale({ maxWidth: 200 }).addTo(map)
 L.control.layers(baseLayers).addTo(map)
